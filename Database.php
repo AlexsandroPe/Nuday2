fcc5cdd65006e0ed5077d51db758ccca169aea05
Alexsandro P. Silva <alexpereirasilv4@gmail.com>
2025-06-16 11:22:07 -0300

    rename identifiers for clarity

--- Database.php
+++ Database.php
@@ -1,66 +1,36 @@
 <?php 
-    class Database {
-        private $dsn = "mysql:host=127.0.0.1;dbname=eventTech";
-        private $user = "root";
-        private $password = "A71BCw!";
-        private $connection;
+class Database {
+    private $dsn = "mysql:host=127.0.0.1;dbname=eventTech";
+    private $user = "root";
+    private $password = "A71BCw!";
+    private $connection;
 
-        public function __construct() 
-        {
-            $this->connection = new PDO($this->dsn, $this->user, $this->password);
-        }
-
-        public function insertPDO($data)
-        {
-            $sql = "INSERT INTO usuarios(name, email, telephone) VALUES(:n, :e, :t)";
-            $res= $this->connection->prepare($sql); // passar parametro e depois substituir o query quando nao precisa substituir
-            
-            //forma de substituicao
-            
-            $res->bindValue(":n", $data->getName());
-            $res->bindValue(":e", $data->getEmail());
-            $res->bindValue(":t", $data->getTel());
-
-            $res->execute();
-        }
-
-        public function getInscritos()
-        {
-            $sql = "SELECT * FROM usuarios";
-            $res= $this->connection->query($sql);
-            return $res;
-        }
-
-        public function deleteInscritos(){}
-
+    public function __construct() 
+    {
+        $this->connection = new PDO($this->dsn, $this->user, $this->password);
     }
 
-
-    // criar uma classe model para realizar isso da maneira mais adequada e organizada
-    // function ConectDB($name, $email, $telephone) {
-    //     $dsn = "mysql:host=127.0.0.1;dbname=eventTech";
-    //     $user = "root";
-    //     $password = "minhasenha!";
-    //     $connection = new PDO($dsn, $user, $password);
+    public function insertUser($user)
+    {
+        $sql = "INSERT INTO usuarios(name, email, telephone) VALUES(:name, :email, :tel)";
+        $stmt = $this->connection->prepare($sql);
         
-    //     $sql = "INSERT INTO usuarios(name, email, telephone) VALUES(:n, :t, :e)";
+        $stmt->bindValue(":name", $user->getName());
+        $stmt->bindValue(":email", $user->getEmail());
+        $stmt->bindValue(":tel", $user->getTel());
 
-    //     // infos vao estar guardada na res e atraves dela sera possivel a substituicao dos parametros
-    //     $res= $connection->prepare($sql); // passar parametro e depois substituir o query quando nao precisa substituir
-   
-    //     //forma de substituicao 
+        $stmt->execute();
+    }
 
-    //     $res->bindValue(":n", $name);
-    //     $res->bindValue(":t", $telephone);
-    //     $res->bindValue(":e", $email);
+    public function getUsers()
+    {
+        $sql = "SELECT * FROM usuarios";
+        $result = $this->connection->query($sql);
+        return $result;
+    }
 
-    //     $res->execute(); // executa a insercao
-    
-    //     // ou bindParam. A diferenca que o param n aceita um valor passado diretamente, somente variaveis e o value aceita tudo;
-
-    //     //ha momentos onde sera mais util usar query momentos como quando precisa passar info diretamente ou tambem quando n tem params pra passar
-
-    //     // $connection->query($sql); // nao precisa do execute();
-    
-    // }
-?>
+    public function deleteUsers()
+    {
+        // Método ainda não implementado
+    }
+}
--- index.php
+++ index.php
@@ -1,24 +1,19 @@
-
 <?php 
-   require_once "autoload.php";
-
+    require_once "autoload.php";
     $dbEventTech = new Database();
-    
+
     if($_SERVER['REQUEST_METHOD'] == "POST") {
         if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['telephone'])) {
             if(!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['telephone'])) {
                 $name = $_POST['name'];
                 $email= $_POST['email'];
                 $telephone = $_POST['telephone'];
-                $inscrito = new User($name, $email, $telephone);
-                $dbEventTech->insertPDO($inscrito);
+                $user = new User($name, $email, $telephone);
+                $dbEventTech->insertUser($user);
                 header("location: index.php");
             }
         }
     }
-    
-
-
 ?>
 
 <!DOCTYPE html>
@@ -31,35 +26,37 @@
 </head>
 <body>
     <main class="main_container">
-        <div class="inscritos">
-            <div class="inscritosHeader">
+        <div class="users">
+            <div class="users_header">
                 <h1>Nuday</h1>
             </div>
-
-            <div class="inscritosContainer">
+            <div class="users_container">
                 <h2 style="color: #fff;">Inscritos</h2>
-                 <?php 
-                foreach($dbEventTech->getInscritos() as $row)
-                {   echo "<div class='inscritoContainer'>
-                     <p>{$row['name']}</p>
-                     <p>{$row['email']}</p>
-                     <p>{$row['telephone']}</p>
-                    </div>";
-                }
-            ?>
+                <?php 
+                    if (empty($dbEventTech->getUsers())) {
+                        foreach ($dbEventTech->getUsers() as $row)
+                        {   echo "<div class='user_card'>
+                            <p>{$row['name']}</p>
+                            <p>{$row['email']}</p>
+                            <p>{$row['telephone']}</p>
+                            </div>";
+                        }
+                    } else { 
+                        echo "<p>Nenhum inscrito até o momento...</p>";
+                    }
+                ?>
             </div>
-           
         </div>  
-        <section class="formSection">
+        <section class="form_section">
             <h2 style="font-size: 40px; justify-self: flex-start; font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;">Oportunidade incrível de fazer networking</h2>
             <p style="font-size: 22px;">Venha participar, se inscrevendo abaixo</p>
-            <form class="forms" action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
-                <div class="form-inputs_container">
+            <form class="form" action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
+                <div class="form_inputs_container">
                     <input type="text" name="name" id="name" placeholder="Nome:">
                     <input type="email" name="email" id="email" placeholder="Email:">
                     <input type="tel" name="telephone" id="telephone" placeholder="Telefone">
                 </div>
-                <input type="submit" value="Inscrever-se" class="submitBtn">
+                <input type="submit" value="Inscrever-se" class="submit_button">
             </form>
         </section>
     </main>
